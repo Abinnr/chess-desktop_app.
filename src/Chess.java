f2b69ce9a86f18180db326e2c5f3eaa955d37e2b
Abinnr <abinnr12345@gmail.com>
2025-06-21 23:51:03 +0530

    initial setup done with chessboard and coins

---
+++ src/Chess.java
@@ -0,0 +1,106 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Chess {
+    JFrame jf;
+    JButton[][] cells;
+    String[][] coins = new String[8][8];
+    public JButton createButton(int x, int y, Color color, JFrame frame) {
+        JButton button = new JButton();
+        button.setBounds(x, y, 50, 50);
+        button.setBackground(color);
+        frame.add(button);
+        return button;
+    }
+
+    public void initializeCoins() {
+        // Black coins. These are uni-code strings for perform backend action logics
+        coins[0][0] = "♜"; coins[0][1] = "♞"; coins[0][2] = "♝"; coins[0][3] = "♛";
+        coins[0][4] = "♚"; coins[0][5] = "♝"; coins[0][6] = "♞"; coins[0][7] = "♜";
+        for (int i = 0; i < 8; i++)
+            coins[1][i] = "♟";
+
+        // White coins
+        coins[7][0] = "♖"; coins[7][1] = "♘"; coins[7][2] = "♗"; coins[7][3] = "♕";
+        coins[7][4] = "♔"; coins[7][5] = "♗"; coins[7][6] = "♘"; coins[7][7] = "♖";
+        for (int i = 0; i < 8; i++)
+            coins[6][i] = "♙";
+    }
+
+    public void ShowCoins() {
+        // Showing Black coins on cells for the - 1st and 2nd rows
+        cells[0][0].setText("♜");
+        cells[0][1].setText("♞");
+        cells[0][2].setText("♝");
+        cells[0][3].setText("♛");
+        cells[0][4].setText("♚");
+        cells[0][5].setText("♝");
+        cells[0][6].setText("♞");
+        cells[0][7].setText("♜");
+
+        for (int i = 0; i < 8; i++) {
+            cells[1][i].setText("♟");
+        }
+
+        //Showing White coins on cells of - 7th and 8th rows
+        cells[7][0].setText("♖");
+        cells[7][1].setText("♘");
+        cells[7][2].setText("♗");
+        cells[7][3].setText("♕");
+        cells[7][4].setText("♔");
+        cells[7][5].setText("♗");
+        cells[7][6].setText("♘");
+        cells[7][7].setText("♖");
+
+        for (int i = 0; i < 8; i++) {
+            cells[6][i].setText("♙");
+        }
+
+        // setting empty the remaining squares initially (2 to 5)
+        for (int i = 2; i <= 5; i++) {
+            for (int j = 0; j < 8; j++) {
+                cells[i][j].setText("");
+                coins[i][j] = null;
+            }
+        }
+    }
+
+
+    public Chess(){
+        jf=new JFrame("Chess multiplayer");
+        jf.setLayout(null);
+        jf.setSize(417,440);
+        jf.setLocation(400,50);
+
+        cells = new JButton[8][8]; // For 8 rows and 8 columns
+
+        int[] yPos = {350, 300,250,200,150,100,50,0}; // Y positions for row 1, row 2,...row 8
+
+        for (int row = 0; row<8; row++) {
+            for (int col = 0; col < 8; col++) {
+                int x = col * 50;
+                int y = yPos[row];
+                if(row % 2 == 0) {
+                    Color color = (col % 2 == 0) ? Color.DARK_GRAY : Color.LIGHT_GRAY;
+                    cells[row][col] = createButton(x, y, color, jf);
+                }else{
+                    Color color = (col % 2 == 0) ? Color.LIGHT_GRAY:Color.DARK_GRAY ;
+                    cells[row][col] = createButton(x, y, color, jf);
+                }
+            }
+        }
+        initializeCoins();
+        ShowCoins();
+
+
+
+
+
+
+
+
+
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
